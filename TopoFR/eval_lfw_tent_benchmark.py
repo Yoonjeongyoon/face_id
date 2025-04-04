8d1c8be50d809b7cfa5d39b5338a839ae3c47483
Yoonjeongyoon <98dabujin@naver.com>
2025-04-04 14:02:36 +0900

    tent test

--- TopoFR/eval_lfw_tent_benchmark.py
+++ TopoFR/eval_lfw_tent_benchmark.py
@@ -413,9 +413,9 @@ if __name__ == '__main__':
     parser.add_argument('--name', default='r50', type=str, help='')
     args = parser.parse_args()
     
-    # 출력 결과를 저장하기 위해 Tee 클래스 사용, 로그 파일명 설정 (원하는 경로로 수정 가능)
-    log_file = os.path.join(os.path.dirname(__file__), 'log/CPLFW_motion_blur_3_benchmark_results.txt')
-    # 콘솔과 파일에 동시 출력하도록 설정
+    # 출력 결과를 저장 하기 위해 Tee 클래스 사용, 로그 파일명 설정 (원하는 경로로 수정 가능)
+    log_file = os.path.join(os.path.dirname(__file__), 'log/CPLFW_gaussian_noise_1_benchmark_results.txt')
+    # 콘솔과 파일에 동시 출력 하도록 설정
     sys.stdout = Tee(log_file)
     
     # 이미지 폴더 경로
